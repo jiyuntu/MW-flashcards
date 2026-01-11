05ad776f74904b605e1378d0dd7b13a2641430ea
jiyuntu <wendy3925@gmail.com>
2026-01-11 18:04:07 +0800

    Rename payload function
    Rename request to make_payload
    for clarity.

--- anki-connect.py
+++ anki-connect.py
@@ -3,12 +3,12 @@ import urllib.request
 
 class AnkiConnect:
     @staticmethod
-    def request(action, **params):
+    def make_payload(action, **params):
         return {'action': action, 'params': params, 'version': 6}
 
     @staticmethod
     def invoke(action, **params):
-        payload = AnkiConnect.request(action, **params)
+        payload = AnkiConnect.make_payload(action, **params)
         requestJson = json.dumps(payload).encode('utf-8')
         response = json.load(urllib.request.urlopen(urllib.request.Request('http://127.0.0.1:8765', requestJson)))
         if len(response) != 2:
